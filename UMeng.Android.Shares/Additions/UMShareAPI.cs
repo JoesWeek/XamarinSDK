6e3a988903d299760f42d29f961a6da795c90332
Qiao Qiao <qiaoqiao0630@163.com>
2017-04-27 11:09:27 +0800

    添加项目文件。

---
+++ UMeng.Android.Shares/Additions/UMShareAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Umeng.Socialize
+{
+    public partial class UMShareAPI
+    {
+        public partial class InitThread : global::Com.Umeng.Socialize.Common.QueuedWork.UMAsyncTask
+        {
+            protected override Java.Lang.Object DoInBackground()
+            {
+                return DoInBackgrounds();
+            }
+        }
+    }
+}
